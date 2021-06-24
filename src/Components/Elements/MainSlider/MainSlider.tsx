f10db9f7233cad85bb814161e01fb60d336c323a
kkwondev <kkwoncokr@gmail.com>
2021-06-24 23:32:51 +0900

    [ADD] MainSlider Component 분리

---
+++ src/Components/Elements/MainSlider/MainSlider.tsx
@@ -0,0 +1,10 @@
+import { MainBannerContainer } from '@Src/Styles/MainStyles';
+import React from 'react';
+
+export default function MainSlider() {
+    return (
+        <MainBannerContainer>
+            <div className="main_banner">banner1</div>
+        </MainBannerContainer>
+    );
+}
