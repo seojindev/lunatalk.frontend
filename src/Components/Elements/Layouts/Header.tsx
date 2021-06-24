3d2736f277a29e228ecef1bba12d577dec48f6f9
kkwondev <kkwoncokr@gmail.com>
2021-06-24 23:13:53 +0900

    [ADD] default Layout add

---
+++ src/Components/Elements/Layouts/Header.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { HeaderContainer } from '@Src/Styles/CommonStyles';
+
+export default function Header() {
+    return (
+        <HeaderContainer>
+            <div className="header">
+                <h1>lunatalk</h1>
+                <div className="nav_container">
+                    <div className="nav">
+                        <ul>
+                            <li>HOME</li>
+                            <li>SHOP</li>
+                            <li>PRODUCTS</li>
+                            <li>PAGES</li>
+                            <li>BLOG</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="icon_container">
+                    <div className="search">
+                        <p>검색</p>
+                    </div>
+                    <div className="my_page">
+                        <p>my</p>
+                    </div>
+                    <div className="cart">
+                        <p>Cart(0)</p>
+                    </div>
+                </div>
+            </div>
+        </HeaderContainer>
+    );
+}
---
+++ src/Components/Elements/index.ts
@@ -0,0 +1,2 @@
+export { default as Footer } from './Layouts/Footer';
+export { default as Header } from './Layouts/Header';
