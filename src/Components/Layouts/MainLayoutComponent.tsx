f4d40f33a6f973a37386bfd355460adacd78b026
kkwondev <kkwoncokr@gmail.com>
2021-10-28 23:22:24 +0900

    [UPDATE] 페이지 이동시 스크롤 초기화

--- src/Components/Layouts/MainLayoutComponent.tsx
+++ src/Components/Layouts/MainLayoutComponent.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MenuBar, FooterBar } from '@Element/Bar';
+import { useLocation } from 'react-router-dom';
 
 export default function MainLayoutComponent({ children }: { children: any }) {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scroll(0, 0);
+    }, [pathname]);
     return (
         <>
             <MenuBar />
